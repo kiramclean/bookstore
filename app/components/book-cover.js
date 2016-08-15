a510303b4e2431315e51bd091183c9e1e853fb7d
Kira McLean <kiraemclean@gmail.com>
2016-08-15 09:20:20 -0400

    add modal for buying book

--- app/components/book-cover.js
+++ app/components/book-cover.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    open() {
+      this.set('isShowingModal', true)
+    },
+    close() {
+      this.set('isShowingModal', false)
+    }
+  }
 });
